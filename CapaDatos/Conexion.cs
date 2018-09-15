d05dbf83ecc8439503f3cbe27f1802a7379ffa45
García Elías Cesar <cyahojasderuta@hotmail.com>
2018-09-14 23:12:26 -0300

    Agregado de Capas

---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    class Conexion
+    {
+        //Establecer la cadena de conexión. Cn = Cadena de Conexión.
+        //Quién va a ser el servidor de datos: Data Source.
+        //Nuestro servidor de datos: GARCIAELIAS-PC\SQLEXPRESS
+        //Nombre de nuestra base de datos: Initial Catalog = dvventas
+        public static string Cn = "Data Source = GARCIAELIAS-PC; Initial Catalog = dvventas; Integrated Security = true";
+    }
+}
