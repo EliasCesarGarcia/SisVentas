7000179591e3bd40b08b60a712a86657dd57fd0a
García Elías Cesar <cyahojasderuta@hotmail.com>
2018-10-07 23:12:44 -0300

    Actualización del frmPresentacion

--- CapaPresentacion/frmPresentacion.Designer.cs
+++ CapaPresentacion/frmPresentacion.Designer.cs
@@ -38,7 +38,6 @@
             this.chkEliminar = new System.Windows.Forms.CheckBox();
             this.btnImprimir = new System.Windows.Forms.Button();
             this.btnEliminar = new System.Windows.Forms.Button();
-            this.btnBuscar = new System.Windows.Forms.Button();
             this.txtBuscar = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.btnCancelar = new System.Windows.Forms.Button();
@@ -55,6 +54,7 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.erroricono = new System.Windows.Forms.ErrorProvider(this.components);
             this.label1 = new System.Windows.Forms.Label();
+            this.btnBuscar = new System.Windows.Forms.Button();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataListado)).BeginInit();
             this.groupBox1.SuspendLayout();
@@ -153,21 +153,13 @@
             this.btnEliminar.Text = "&Eliminar";
             this.btnEliminar.UseVisualStyleBackColor = true;
             // 
-            // btnBuscar
-            // 
-            this.btnBuscar.Location = new System.Drawing.Point(252, 33);
-            this.btnBuscar.Name = "btnBuscar";
-            this.btnBuscar.Size = new System.Drawing.Size(78, 19);
-            this.btnBuscar.TabIndex = 2;
-            this.btnBuscar.Text = "&Buscar";
-            this.btnBuscar.UseVisualStyleBackColor = true;
-            // 
             // txtBuscar
             // 
             this.txtBuscar.Location = new System.Drawing.Point(71, 33);
             this.txtBuscar.Name = "txtBuscar";
             this.txtBuscar.Size = new System.Drawing.Size(145, 20);
             this.txtBuscar.TabIndex = 1;
+            this.txtBuscar.TextChanged += new System.EventHandler(this.txtBuscar_TextChanged);
             // 
             // label2
             // 
@@ -204,6 +196,7 @@
             this.btnGuardar.TabIndex = 7;
             this.btnGuardar.Text = "&Guardar";
             this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
             // 
             // btnNuevo
             // 
@@ -213,6 +206,7 @@
             this.btnNuevo.TabIndex = 6;
             this.btnNuevo.Text = "&Nuevo";
             this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
             // 
             // txtDescripcion
             // 
@@ -310,22 +304,36 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.label1.Location = new System.Drawing.Point(23, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(180, 29);
             this.label1.TabIndex = 2;
             this.label1.Text = "Presentaciones";
             // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(252, 33);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(78, 19);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "&Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
             // frmPresentacion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(740, 457);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
             this.Name = "frmPresentacion";
             this.Text = "Mantenimiento de Presentaciones";
+            this.Load += new System.EventHandler(this.frmPresentacion_Load);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataListado)).EndInit();
--- CapaPresentacion/frmPresentacion.cs
+++ CapaPresentacion/frmPresentacion.cs
@@ -8,13 +8,261 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//Me comunico con la Capa Negocio
+using CapaNegocio;
+
 namespace CapaPresentacion
 {
     public partial class frmPresentacion : Form
     {
+        //2 Variables. Me van a indicar si voy a registrar un artículo o lo voy a editar.
+        private bool IsNuevo = false;
+        private bool IsEditar = false;
+
         public frmPresentacion()
         {
+            //Este método inicializa todos los controles:
             InitializeComponent();
+            //Mostrar el mensaje de ayuda. Tenemos un control: ttMensaje
+            //LLamamos a su método: SetToolTip
+            //Parámetros: txtNombre que es el textbox. También el texto a mostrar.
+            this.ttMensaje.SetToolTip(this.txtNombre, "Ingrese el Nombre de la Presentación");
+        }
+
+        //Método para Mostrar Mensaje de Confirmación:
+        private void MensajeOk(string mensaje)
+        {
+            //Mostrar primero el mensaje, parámetro: mensaje.
+            //Mostrará como título: "Sistema de Ventas".
+            //Mostrará un botón: MessageBoxButtons.OK.
+            //Mostrará un ícono: MessageBoxIcon.Information.
+            MessageBox.Show(mensaje, "Sistema de Ventas", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        //Método para Mostrar Mensaje de Error:
+        private void MensajeError(string mensaje)
+        {
+            //Mostrar primero el mensaje, parámetro: mensaje.
+            //Mostrará como título: "Sistema de Ventas".
+            //Mostrará un botón: MessageBoxButtons.OK.
+            //Mostrará un ícono: MessageBoxIcon.Error.
+            MessageBox.Show(mensaje, "Sistema de Ventas", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        //Método para Limpiar todos los controles del formulario:
+        private void Limpiar()
+        {
+            //Caja de texto: txtNombre.
+            //Para dejar en blanco: string.Empty.
+            this.txtNombre.Text = string.Empty;
+            //Caja de texto: txtDescripcion.
+            this.txtDescripcion.Text = string.Empty;
+            //Caja de texto: txtIdcategoria.
+            this.txtIdpresentacion.Text = string.Empty;
+        }
+
+        //Habilitar los controles de los formularios:
+        private void Habilitar(bool valor)
+        {
+            //Este procedimiento va modificar las cajas de texto, para que estén habilitadas o
+            //deshabilitadas.
+
+            //ReadOnly: para hacerla de sólo lectura.
+            //!Valor sería falso, negamos el valor que recibimos.  
+            //Si recibimos true las cajas de texto van a estar habilitadas.
+            //Si recibimos false, las cajas de texto van a estar deshabilitadas.
+            this.txtNombre.ReadOnly = !valor;
+            this.txtDescripcion.ReadOnly = !valor;
+            this.txtIdpresentacion.ReadOnly = !valor;
+        }
+
+        //Habilitar los botones:
+        private void Botones()
+        {
+            //Éste procedimiento va a habilitar o deshabilitar ciertos botonos.
+            //Si IsNuevo está activado, es decir si se va a registrar un artículo...
+            //O está activado IsEditar:
+            if (this.IsNuevo || this.IsEditar)
+            {
+                //LLamamos a nuesto método Habilitar para las cajas de texto:
+                this.Habilitar(true);
+                //Deshabilite el botón:
+                this.btnNuevo.Enabled = false;
+                //Habilitamos el botón guardar:
+                this.btnGuardar.Enabled = true;
+                //Dehabilitamos el botón editar:
+                this.btnEditar.Enabled = false;
+                //Habilitamos el botón cancelar:
+                this.btnCancelar.Enabled = true;
+            }
+            else
+            {
+                //Si no está insertando o editando un registro, deshabilitamos las cajas de texto: false.
+                this.Habilitar(false);
+                //Habilitamos el botón Nuevo, para que pueda registrar el artículo:
+                this.btnNuevo.Enabled = true;
+                //Deshabilitamos el botón guardar, ya que no ingresamos nada, no puede guardar.
+                this.btnGuardar.Enabled = false;
+                //Habilitamos el botón editar:
+                this.btnEditar.Enabled = true;
+                //Habilitamos el botón cancelar:
+                this.btnCancelar.Enabled = false;
+            }
+        }
+
+        //Método para ocultar columnas:
+        private void OcultarColumnas()
+        {
+            //False: para que no esté visible:
+            //Oculto la columna: Eliminar [0]
+            //Oculto la columna: idcategoria [1]
+            this.dataListado.Columns[0].Visible = false;
+            this.dataListado.Columns[1].Visible = false;
+        }
+
+        //Método Mostrar todos los registros de la tabla presentacion:
+        private void Mostrar()
+        {
+            //LLamo a mi clase NPresentacion, donde está mi procedimiento: Mostrar, para que me envíe...
+            //... ciertos valores.
+            this.dataListado.DataSource = NPresentacion.Mostrar();
+            this.OcultarColumnas();
+            //lo concatenamos con el total de registros, llamando a su métodos Rows (de filas)...
+            //... y llamamos a su método count, para contar todas las filas.
+            //Como: ataListado.Rows.Count, me devuelve un int y lo que queremos es un string...
+            //... para eso usamos: Convert.ToString, para convertir todo a un string:
+            lblTotal.Text = "Total de Registros: " + Convert.ToString(dataListado.Rows.Count);
+        }
+
+        //Método BuscarNombre.
+        private void BuscarNombre()
+        {
+            //LLamo a mi clase NPresentacion, donde está mi procedimiento: BuscarNombre, para que me envíe...
+            //... ciertos valores.
+            //BuscarNombre, está esperando un parámetro: la caja de texto: txtBuscar
+            //Y obtengo el texto con su propiedad: .Text
+            this.dataListado.DataSource = NPresentacion.BuscarNombre(this.txtBuscar.Text);
+            this.OcultarColumnas();
+            //lo concatenamos con el total de registros, llamando a su métodos Rows (de filas)...
+            //... y llamamos a su método count, para contar todas las filas.
+            //Como: ataListado.Rows.Count, me devuelve un int y lo que queremos es un string...
+            //... para eso usamos: Convert.ToString, para convertir todo a un string:
+            lblTotal.Text = "Total de Registros: " + Convert.ToString(dataListado.Rows.Count);
+        }
+
+        private void frmPresentacion_Load(object sender, EventArgs e)
+        {
+            //Primero, ubicar nuestro formulario:
+            //En la esquina superior:
+            this.Top = 0;
+            //Alineado en la izquierda:
+            this.Left = 0;
+
+            this.Mostrar();
+            //Le digo que las cajas de texto no estén habilitadas o sean de sólo lectura:
+            this.Habilitar(false);
+            this.Botones();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            this.BuscarNombre();
+        }
+
+        private void txtBuscar_TextChanged(object sender, EventArgs e)
+        {
+            //Cada vez que el usuario escriba algo, envíe la petición a la capa Negocio, la Capa Negocio...
+            //... a la Capa Datos y se filtre en nuestra Base de Datos.
+            this.BuscarNombre();
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            //Cuando se haga clic en este botón (IsNuevo), se va registrar un artículo.
+            //La variable IsNuevo, se va a convertir en verdadera:
+            this.IsNuevo = true;
+            //No voy a editar, sino a registrar un artículo, por eso será falsa:
+            this.IsEditar = false;
+            this.Botones();
+            this.Limpiar();
+            //Habilitar las cajas de texto con true:
+            this.Habilitar(true);
+            //Enviar el enfoque a ésta caja de texto:
+            this.txtNombre.Focus();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Declaro una variable para evaluar si se insertó o modificó:
+                string rpta = "";
+                //Voy a validar los datos. En éste caso, el campo Nombre no puede estar vacío:
+                //string.Empty = quiere decir que está vacía:
+                if (this.txtNombre.Text == string.Empty)
+                {
+                    MensajeError("Falta ingresar algunos datos, serán remarcados");
+                    //Que el icono de error aparezca al costado de la caja de texto: txtNombre.
+                    erroricono.SetError(txtNombre, "Ingrese un Nombre");
+                }
+                else
+                {
+                    //El usuario quiere realizar un registro:
+                    if (this.IsNuevo)
+                    {
+                        //Se envían 2 parámatros: txtNombre, para enviar el nombre. Y descripción.
+                        //Trim: para borrar los espacios en blanco.
+                        //ToUpper: para convertir a mayúsculas.
+                        rpta = NPresentacion.Insertar(this.txtNombre.Text.Trim().ToUpper(),
+                                                   this.txtDescripcion.Text.Trim());
+                    }
+                    else
+                    {
+                        //Se envían 3 parámatros.
+                        //Como es un string: this.txtIdpresentacion.Text, lo paso a un int: Convert.ToInt32
+                        //Trim: para borrar los espacios en blanco.
+                        //ToUpper: para convertir a mayúsculas.
+                        rpta = NPresentacion.Editar(Convert.ToInt32(this.txtIdpresentacion.Text),
+                                                 this.txtNombre.Text.Trim().ToUpper(),
+                                                 this.txtDescripcion.Text.Trim());
+                    }
+
+                    //Esto es para la respuesta (rpta) de DCategoria, en el método insertar:
+                    //Equals: para comparar una cadena.
+                    if (rpta.Equals("OK")) //OK, es si se insertó o modificó.
+                    {
+                        //Recibo un OK del insertr, porque IsNuevo está en true en el if:
+                        if (this.IsNuevo)
+                        {
+                            this.MensajeOk("se insertó de forma correcta el registro");
+                        }
+                        //Si no es Insertar(IsNuevo) es un editar:
+                        else
+                        {
+                            this.MensajeOk("se actualizó de forma correcta el registro");
+                        }
+                    }
+                    //Si No recibo un OK:
+                    else
+                    {
+                        //rpta, es el que recibe los mensajes.
+                        this.MensajeError(rpta);
+                    }
+
+                    //Lo dejó en false, porque ya ingresé el registro:
+                    this.IsNuevo = false;
+                    this.IsEditar = false;
+                    this.Botones();
+                    this.Limpiar();
+                    //Para Mostrar actualizado nuestro datalistado:
+                    this.Mostrar();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace);
+            }
         }
     }
 }
